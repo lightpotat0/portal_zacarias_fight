687cbe9d3d94da611f4c2e75e084b3eb5844935f
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-05 14:48:31 -0300

    config the clicker function

--- scripts/black.gd
+++ scripts/black.gd
@@ -10,11 +10,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
 		var sprite = $"../Black1"
-		if sprite.visible and not clicado:
-			var texture = sprite.sprite_frames.get_frame_texture(sprite.animation, sprite.frame)
-			var rect = Rect2(-texture.get_size() / 2, texture.get_size())
-			if rect.has_point(sprite.get_local_mouse_position()):
-				botao_clicado()
+		botao_clicado()
 		
 func mouse_entrou():
 	if clicado: return
