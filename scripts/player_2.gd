2a1bc539c7340fb9b6d7acdc6614abd8eb754e98
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-13 14:02:19 -0300

    player2 move and new sprites

---
+++ scripts/player_2.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $Moves
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		_animated_sprite.flip_h = direction > 0
+		_animated_sprite.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animated_sprite.play("walk")
+
+	move_and_slide()
+	
+	if not is_on_floor():
+		_animated_sprite.play("jump") 
+	elif direction != 0:
+		_animated_sprite.play("run")  
+	else:
+		_animated_sprite.play("idle")
