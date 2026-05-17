b464358ad935cbe51530523523afe502779a3804
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-17 17:44:00 -0300

    new animated moves

--- scripts/player_2.gd
+++ scripts/player_2.gd
@@ -3,17 +3,22 @@ extends CharacterBody2D
 @onready var _animated_sprite = $Moves
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -1300.0
+const GRAVITY_SCALE = 3
+const FALL_GRAVITY_SCALE = 5.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
-		_animated_sprite.offset = Vector2(0, -10)
+		if velocity.y < 0:
+			velocity.y += gravity * GRAVITY_SCALE * delta
+		else:
+			velocity.y += gravity * FALL_GRAVITY_SCALE * delta
 
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		_animated_sprite.play("jump") 
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -22,6 +27,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		_animated_sprite.play("walk")
+	if Input.is_action_just_pressed("ui_down") and is_on_floor():
+		_animated_sprite.play("shift")
 
 	move_and_slide()
 	
