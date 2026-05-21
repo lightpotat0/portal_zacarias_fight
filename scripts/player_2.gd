a8783acd302ceb9d88c04b6cfad7df401f370b7d
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-21 14:08:41 -0300

    organizando

--- scripts/player_2.gd
+++ scripts/player_2.gd
@@ -18,27 +18,23 @@ func _ready():
 	ajustar_colisao_ao_sprite()
 
 func _physics_process(delta):
-	# Gravidade
 	if not is_on_floor():
 		if velocity.y < 0:
 			velocity.y += gravity * GRAVITY_SCALE * delta
 		else:
 			velocity.y += gravity * FALL_GRAVITY_SCALE * delta
 
-	# Pulo e Ataques Aéreos (Detecta se acabou de apertar o pulo)
 	if Input.is_action_just_pressed("x") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		_animated_sprite.play("jump")
 		agachado = false
 	
-	# Ataques no ar (Mudado para 'is_action_pressed' para o caso de segurar o botão antes)
 	if not is_on_floor():
 		if Input.is_action_pressed("quadrado"):
 			_animated_sprite.play("jump_punch")	
 		elif Input.is_action_pressed("triangulo"):
 			_animated_sprite.play("jump kick")	
 		
-	# Agachar (Funciona enquanto mantiver pressionado)
 	if Input.is_action_pressed("down") and is_on_floor():
 		agachado = true
 		if Input.is_action_pressed("triangulo"):
@@ -46,7 +42,6 @@ func _physics_process(delta):
 	else:
 		agachado = false
 		
-	# Ataques e Bloqueio no Chão (Mudado para 'is_action_pressed' para funcionar enquanto segura)
 	if is_on_floor() and not agachado:
 		if Input.is_action_pressed("quadrado"): 
 			atacando = true
@@ -64,7 +59,6 @@ func _physics_process(delta):
 			atacando = false
 			bloqueando = false
 
-	# Movimentação Horizontal (Apenas se não estiver defendendo ou atacando no chão)
 	var direction := Input.get_axis("left", "right")
 	
 	if direction != 0 and not bloqueando and not (atacando and is_on_floor()):
@@ -75,9 +69,7 @@ func _physics_process(delta):
 		
 	move_and_slide()
 	
-	# Máquina de Estados de Animação
 	if not is_on_floor():
-		# Se já não estiver passando a animação de soco/chute aéreo, usa o pulo padrão
 		if _animated_sprite.animation != "jump_punch" and _animated_sprite.animation != "jump kick":
 			_animated_sprite.play("jump")
 		_animated_sprite.scale = Vector2(1.3, 1.3)
