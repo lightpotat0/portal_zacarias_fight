8e8df5fe16fe81586451ddce4d26203fcfec3b83
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-22 10:41:57 -0300

    scene and camera configs

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+@onready var target = get_node("../Player2")
+@export var smooth_speed: float = 5.0
+
+func _ready() -> void:
+	limit_left = -500    
+	limit_right = 1680
+	
+func _process(delta: float) -> void:
+	if target:
+		var target_position = global_position
+		target_position.x = target.global_position.x
+		global_position = global_position.lerp(target_position, smooth_speed * delta)
