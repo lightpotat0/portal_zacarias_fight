14c9bb5f74da85412293fc4b793de9f62fdd949a
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-05-24 09:24:56 -0300

    sprite scale

--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -56,7 +56,7 @@ func _physics_process(delta):
 
 	if direction != 0:
 		velocity.x = direction * SPEED
-		_animated_sprite.flip_h = direction > 0
+		_animated_sprite.flip_h = direction < 0
 	if direction != 0 and not bloqueando and not agachado and not (atacando and is_on_floor()):
 		velocity.x = direction * SPEED
 	else:
@@ -67,11 +67,11 @@ func _physics_process(delta):
 	if not is_on_floor():
 		if Input.is_action_pressed("quadrado"):
 			_animated_sprite.play("jump_punch")
-			_animated_sprite.scale = Vector2(1.6, 1.6)
+			_animated_sprite.scale = Vector2(1.0, 1.0)
 			_animated_sprite.offset = Vector2(0, -15)
 		elif Input.is_action_pressed("triangulo"):
-			_animated_sprite.play("jump kick")
-			_animated_sprite.scale = Vector2(0.8, 0.8)
+			_animated_sprite.play("jump_kick")
+			_animated_sprite.scale = Vector2(1.0, 1.0)
 			_animated_sprite.offset = Vector2(0, -15)
 		else:
 			_animated_sprite.play("jump")
@@ -82,15 +82,19 @@ func _physics_process(delta):
 		_animated_sprite.offset = Vector2(0, 0)
 
 		if agachado:
-			_animated_sprite.scale = Vector2(1.0, 1.0)
+			_animated_sprite.scale = Vector2(0.8, 0.8)
 			_animated_sprite.offset = Vector2(0, 100)
 
 			if Input.is_action_pressed("triangulo"):
 				_animated_sprite.play("shift_kick")
+				_animated_sprite.scale = Vector2(0.45, 0.45)
+				_animated_sprite.offset = Vector2(0, 250)
 			elif Input.is_action_pressed("o"):
 				_animated_sprite.play("shift_block")
+				_animated_sprite.scale = Vector2(0.5, 0.5)
 			elif Input.is_action_pressed("quadrado"):
 				_animated_sprite.play("shift_punch")
+				_animated_sprite.scale = Vector2(1.0, 1.0)
 			else:
 				_animated_sprite.play("shift")
 		elif atacando:
@@ -99,6 +103,7 @@ func _physics_process(delta):
 			_animated_sprite.play(animacao_ataque)
 		elif direction != 0:
 			_animated_sprite.play("walk")
+			_animated_sprite.scale = Vector2(2.0, 2.0)
 		else:
 			_animated_sprite.play("stop")
 
